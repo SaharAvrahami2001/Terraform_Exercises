9d03ea981ea45fa64638e66f087a8faa0d23ed4a
Sahar_Avrahami <113514105+SaharAvrahami2001@users.noreply.github.com>
2024-09-20 17:44:58 +0300

    add security group resource to exc3

--- exc3/main.tf
+++ exc3/main.tf
@@ -3,12 +3,23 @@ resource "aws_key_pair" "terra_key" {
   public_key = file("terrakey.pub")
 }
 
+resource "aws_security_group" "web_sg" {
+  name        = "web_security_group"
+  description = "Allow inbound traffic on port 80"
+
+  inbound_rule {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow traffic from anywhere
+  }
+
 resource "aws_instance" "terra_instance" {
   ami                    = var.AMIS[var.REGION]
   instance_type          = "t2.micro"
   availability_zone      = var.ZONE1
   key_name               = aws_key_pair.terra_key.key_name
-  vpc_security_group_ids = ["sg-0bd9870dd88007feb"]
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
   tags = {
     Name    = "Terra-Instance-3"
     Project = "Terraform"
